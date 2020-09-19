c0a9a169491602fe26f981e665ee0df93ecaa52b
Paulo Renato Schorn <paulo.schorn@ntconsultcorp.com>
2020-09-19 15:50:09 -0300

    organizing files

--- services/file_services.py
+++ services/file_services.py
@@ -3,5 +3,5 @@ import json
 
 
 def save_json(data, filename: string):
-    file = open(filename, 'w')
+    file = open(f"output/{filename}", 'w')
     file.write(json.dumps(data, indent=4, sort_keys=True))
